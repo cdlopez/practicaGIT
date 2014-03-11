1db1c116b8c854cea39c8a0dd2e640bc6610f83d
cdlopez <cdlopez@alu.uma.es>
2014-03-11 19:48:52 +0100

    Somos tres

--- GitAux.java
+++ GitAux.java
@@ -1,9 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-package prgitejemplo;
-
 /**
  *
  * @author bertoa
@@ -16,6 +10,10 @@ public class GitAux {
     
     public void metodoAlumno2(){
         System.out.println("metodo 2");
+    }
+ 
+    public void metodoAlumno3(){
+        System.out.println("metodo 3");
     }
     
     public void metodoComunitario(){
