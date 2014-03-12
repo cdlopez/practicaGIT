873686995a0c5ac2c1ece1411ce6f2c443f5aff7
IgnacioM <nacho988@gmail.com>
2014-03-12 20:09:32 +0100

    modificaciones alumno3

--- GitAux.java
+++ GitAux.java
@@ -10,16 +10,16 @@ public class GitAux {
     
     public void metodoAlumno2(){
         System.out.println("metodo 2");
-	System.out.println("modificado por alumno 2"); // alumno2
     }
  
     public void metodoAlumno3(){
         System.out.println("metodo 3");
+        System.out.println("modificado por alumno 3"); // alumno3
     }
     
     public void metodoComunitario(){
         System.out.println("Aqui escribimos todos");
 	//modificaciones alumno2
-	System.out.println("Alumno 2 inserta esta sentencia");
+	System.out.println("Alumno 3 inserta esta sentencia");
     }
 }
